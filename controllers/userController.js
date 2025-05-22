cb4fbee952c89c8fbfcb7fa0ae48fe28d15d33f6
PasanSasmika <pasansasmika198@gmail.com>
2025-05-22 10:14:17 +0530

    🏌️‍♀️Signup and login functions

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,8 @@
 import User from "../models/usersModel.js"
 import bcrypt from 'bcrypt';
-
-
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken'
+dotenv.config();
 
 export function creatAdmin(req,res){
 
@@ -40,4 +41,100 @@ user.save().then(()=>{
     })
 })
 
+}
+
+
+
+export function createCustomer(req, res) {
+    const newUserData = req.body;
+
+    User.findOne({ email: newUserData.email })
+        .then(existingUser => {
+            if (existingUser) {
+                return res.status(400).json({ message: "A user with this email already exists." });
+            }
+
+            newUserData.password = bcrypt.hashSync(newUserData.password, 10);
+
+            const user = new User(newUserData);
+
+            user.save()
+                .then(savedUser => {
+                    const token = jwt.sign({
+                        email: savedUser.email,
+                        firstName: savedUser.firstName,
+                        lastName: savedUser.lastName,
+                        isBlocked: savedUser.isBlocked,
+                        type: savedUser.type,
+                        profilepic: savedUser.profilepic
+                    }, process.env.SECRET);
+
+                    res.status(201).json({
+                        message: "Registration successful",
+                        token: token,
+                        user: {
+                            firstName: savedUser.firstName,
+                            lastName: savedUser.lastName,
+                            email: savedUser.email,
+                            profilepic: savedUser.profilepic,
+                            type: savedUser.type
+                        }
+                    });
+                })
+                .catch(err => {
+                    console.error("Error creating user:", err);
+                    res.status(500).json({ message: "Customer user not created", error: err });
+                });
+        })
+        .catch(err => {
+            console.error("Error checking email:", err);
+            res.status(500).json({ message: "Error occurred while checking email", error: err });
+        });
+}
+
+
+
+export function loginUser(req,res){
+
+    User.find({email: req.body.email}).then(
+        (users)=>{
+            if(users.length == 0){
+                res.json({
+                    message: "User not found"
+                })
+            }else {
+
+                const user = users[0]
+
+               const isPasswordCorrect =  bcrypt.compareSync(req.body.password, user.password)
+
+               if(isPasswordCorrect){
+                const token = jwt.sign({
+                    email: user.email,
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                    isBlocked: user.isBlocked,
+                    type: user.type,
+                    profilepic: user.profilepic
+                }, process.env.SECRET)
+                    res.json({
+                        message: "Logged in successful",
+                        token: token,
+                        user: {
+                            firstName : user.firstName,
+                            lastName : user.lastName,
+                            email: user.email,
+                            profilepic: user.profilepic,
+                            type : user.type
+                        }
+                    })
+
+               }else{
+                res.json({
+                    message: "Your Password Is not correct"
+                })
+               }
+            }
+        }
+    )
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { creatAdmin } from '../controllers/userController.js';
+import { creatAdmin, createCustomer, loginUser } from '../controllers/userController.js';
 
 const userRouter = express.Router();
 
 userRouter.post("/admin",creatAdmin);
+userRouter.post("/signup",createCustomer);
+userRouter.post("/login",loginUser);
 
 
 export default userRouter;
