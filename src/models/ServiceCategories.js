270a12e9c7c7a918c364be2a6803caa5bc174506
PasanSasmika <pasansasmika198@gmail.com>
2025-09-02 22:10:46 +0530

    update

--- src/models/ServiceCategories.js
+++ src/models/ServiceCategories.js
@@ -1,23 +1,28 @@
 import mongoose from "mongoose";
 
 const ServiceCategorySchema = mongoose.Schema({
-
     Category: {
-
         type: String,
-        required : true,
+        required: true,
     },
+    subCategories: [
+        {
+            name: {
+                type: String,
+                required: true
+            },
+            description: {
+                type: String,
+                required: false
+            },
+            icon: {
+                type: String,
+                required: false
+            }
+        }
+    ]
+});
 
-    subCategories :[
-         {
-            
-          type : String,
-          required : true
-       }
-    ],
-    
-})
-
-const ServiceCategories = mongoose.model("serviceCategories", ServiceCategorySchema)
+const ServiceCategories = mongoose.model("serviceCategories", ServiceCategorySchema);
 
 export default ServiceCategories;
